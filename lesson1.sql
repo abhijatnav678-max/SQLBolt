48d4da8ef41b1b7578e790e923ad636e13bbfde0
abhijatnav678-max <abhijatnav678@gmail.com>
2026-09-11 14:00:08 +0530

    Completed SQLBolt Lesson 1

---
+++ lesson1.sql
@@ -0,0 +1,23 @@
+
+-- SQLBolt Lesson 1
+
+
+--question 1: Find the title of each film
+SELECT title
+FROM movies;
+
+--question 2:Find the director of each film
+SELECT director
+FROM movies;
+
+--question 3:Find the title and director of each film
+SELECT director,title
+FROM movies;
+
+--question 4:Find the title and year of each film
+SELECT title,year
+FROM movies;
+
+-- question 5:Find all the information about each film
+SELECT * 
+from movies;
