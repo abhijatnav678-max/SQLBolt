5686ad62bc92402c2c42b735844fce0810d0ef2a
abhijatnav678-max <abhijatnav678@gmail.com>
2026-09-12 12:00:34 +0530

    Completed SQLBolt Lesson 7

---
+++ lesson7.sql
@@ -0,0 +1,16 @@
+
+-- SQLBolt Lesson 7
+
+--question 1:Find the list of all buildings that have employees
+SELECT DISTINCT building 
+FROM employees;
+
+--question 2:Find the list of all buildings and their capacity
+SELECT *
+FROM buildings;
+
+--question 3:List all buildings and the distinct employee roles in each building (including empty buildings)
+SELECT DISTINCT building_name, role 
+FROM buildings 
+  LEFT JOIN employees
+    ON building_name = building;
